2ba5470ede1c9df8aa060f45f22e887a4dbc0e58
Trystan <loke1118@hotmail.com>
2024-08-05 10:38:42 -0400

    feat: Enable CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import boto3
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 
 app = Flask(__name__)
+CORS(app)
 
 # Initialize the DynamoDB client
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')  # Replace 'your-region' with your actual region
